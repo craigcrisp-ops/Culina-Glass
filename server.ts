9faa53d7555b24a72758bd49888a63089155790c
craigcrisp-ops <craigcrisp@fortress-apps.com>
2026-03-02 12:31:24 -0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -79,9 +79,10 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
-  });
+ const googlePort = Number(process.env.PORT) || 8080;
+app.listen(googlePort, "0.0.0.0", () => {
+  console.log(`Server running on port ${googlePort}`);
+});
 }
 
 startServer();
